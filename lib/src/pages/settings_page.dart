f7778e01edf3350d0c4056d031e5cf2628ce0349
José Ángel <josnih@gmail.com>
2020-06-01 14:37:12 +0100

    refactor

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -58,7 +58,7 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
           RadioListTile(
             value: 2,
-            title: Text('Femenine'), 
+            title: Text('Feminine'), 
             groupValue: _genre, 
             onChanged: _setSelectedRadio,
           ),
@@ -67,8 +67,8 @@ class _SettingsPageState extends State<SettingsPage> {
             child: TextField(
               controller: _textController,
               decoration: InputDecoration(
-                labelText: 'Nombre',
-                helperText: 'Nombre de la persona usando el telefono'
+                labelText: 'Name',
+                helperText: 'Introduce your name for save preferences'
               ),
               onChanged: (value){
                 prefs.userName = value;
